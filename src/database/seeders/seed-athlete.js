6fb8d90b906be1a7e365b7f6187ea2397f2fca02
Eko Mirhard <eko.mirhard@gmail.com>
2019-08-27 23:28:12 +0700

    Add LUT for position

---
+++ src/database/seeders/seed-athlete.js
@@ -0,0 +1,23 @@
+const { currentDateTime } = require('../../util/date');
+const { Athlete } = require('../models');
+
+module.exports = {
+  up: async (queryInterface) => {
+    // Only seed the table if it is empty
+    const empty = !(await Athlete.count());
+
+    if (empty) {
+      return queryInterface.bulkInsert('Athletes', [{
+        name: 'Spencer Dinwiddie',
+        teamId: 1,
+        positionId: 1,
+        createdAt: currentDateTime,
+        updatedAt: currentDateTime,
+      }], {});
+    }
+  },
+
+  down: queryInterface => (
+    queryInterface.bulkDelete('Athletes', null, {})
+  ),
+};
