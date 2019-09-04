0c5b77212f13f383a18c83794384044d4b3a3b30
Eko Mirhard <eko.mirhard@gmail.com>
2019-09-04 21:55:50 +0700

    Cleanup

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,20 +1,9 @@
 const { Router } = require('express');
 const routeHandler = require('../middleware/route-handler');
-// const identifyUser = require('../middleware/identify-user');
-const {
-  // fetchUser,
-  authenticateUser,
-  // updateUserProfile,
-} = require('../controllers');
+const { authenticateUser } = require('../controllers');
 
 const router = Router();
 
 router.post('/authenticate', routeHandler(authenticateUser));
-
-// Requires authenticated user from this point on
-// router.use('/', identifyUser);
-
-// router.post('/', routeHandler(fetchUser));
-// router.post('/update', routeHandler(updateUserProfile));
 
 module.exports = router;
