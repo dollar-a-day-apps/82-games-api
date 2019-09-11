13bae87016795ba22d40b4a646f7b0dac73ba909
Eko Mirhard <eko.mirhard@gmail.com>
2019-09-12 00:14:41 +0700

    Expand game endpoint

--- src/controllers/game.js
+++ src/controllers/game.js
@@ -1,12 +1,54 @@
+const moment = require('moment-timezone');
 const {
   Sequelize: { Op },
   Game,
+  GameStatistic,
 } = require('../database/models');
 const throwError = require('../util/throw-error');
 const validateInput = require('../joi/validate-input');
 const { sanitizeList } = require('../util/sanitize-output');
 const { fetchGamesByTeamIdSchema } = require('../joi/methods');
 const { routeErrorMessages } = require('../util/constants');
+const { seasonTable } = require('../util/look-up-tables');
+
+// Attach the season identifier for each games
+// and then grab the final point count for each teams from the stats
+const appendMetadata = (games) => {
+  const result = [];
+
+  games.forEach((game) => {
+    const {
+      dateTime,
+      'GameStatistic.homeTeamPoints': homeTeamPoints,
+      'GameStatistic.awayTeamPoints': awayTeamPoints,
+    } = game;
+    const gameDateTime = moment(dateTime).tz('America/New_York');
+
+    let gameSeason = null;
+    seasonTable.forEach((season) => {
+      const seasonStartDate = moment.tz(season.startDate, 'America/New_York');
+      const seasonEndDate = moment.tz(season.endDate, 'America/New_York');
+
+      if (gameDateTime.isBetween(seasonStartDate, seasonEndDate, 'day', '[]')) {
+        gameSeason = season;
+      }
+    });
+
+    if (gameSeason) {
+      result.push({
+        ...game,
+        season: gameSeason.id,
+        homeTeamPoints,
+        awayTeamPoints,
+      });
+    } else {
+      // Should never come down here unless we have a unindexed/invalid game record
+      console.log('Invalid Game Data');
+    }
+  });
+
+  return result;
+};
 
 module.exports = {
   fetchGamesByTeamId: async (req) => {
@@ -25,10 +67,24 @@ module.exports = {
             awayTeamId: teamId,
           }],
         },
+        include: [GameStatistic],
         raw: true,
       });
 
-      return sanitizeList(games, ['referenceId']);
+      // Attach season identifier and also return the final result/points from both teams
+      // then remove unrelevant fields
+      const sanitizedGames = sanitizeList(appendMetadata(games), [
+        'referenceId',
+        'GameStatistic.id',
+        'GameStatistic.gameId',
+        'GameStatistic.homeTeamPoints',
+        'GameStatistic.awayTeamPoints',
+        'GameStatistic.homeTeamStatistics',
+        'GameStatistic.awayTeamStatistics',
+        'GameStatistic.createdAt',
+        'GameStatistic.updatedAt',
+      ]);
+      return sanitizedGames;
     } catch (err) {
       return throwError(new Error(routeErrorMessages.FETCH_GAMES_FAILED), {
         fn: 'fetchGamesByTeamId',
