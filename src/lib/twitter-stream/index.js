22a15077b009e359856eb843184cb45259152d8a
Eko Mirhard <eko.mirhard@gmail.com>
2019-09-26 23:47:08 +0700

    Use better twitter package and apply filter

--- src/lib/twitter-stream/index.js
+++ src/lib/twitter-stream/index.js
@@ -1,4 +1,4 @@
-const TwitterStream = require('twitter-stream-api');
+const Twitter = require('twitter');
 const { updateCachedTweetsByAthleteId } = require('../redis/twitter');
 const throwError = require('../../util/throw-error');
 
@@ -12,8 +12,8 @@ const {
 const credentials = {
   consumer_key: TWITTER_CONSUMER_KEY,
   consumer_secret: TWITTER_CONSUMER_SECRET,
-  token: TWITTER_ACCESS_TOKEN,
-  token_secret: TWITTER_ACCESS_TOKEN_SECRET,
+  access_token_key: TWITTER_ACCESS_TOKEN,
+  access_token_secret: TWITTER_ACCESS_TOKEN_SECRET,
 };
 
 const handleSetupError = (err) => {
@@ -25,62 +25,61 @@ const handleSetupError = (err) => {
 
 // Initiate the tweet streaming from Twitter API
 const setupTwitterStream = () => {
-  const Twitter = new TwitterStream(credentials, false);
+  const client = new Twitter(credentials);
+  const mainTwitterId = '368148234';
 
-  // Stream only for latest tweets from Spencer's account for now
-  Twitter.stream('statuses/filter', {
-    // track: 'nba',
-    follow: ['368148234'],
-  });
+  // Setup the streamer and follow Spencer's account
+  const stream = client.stream('statuses/filter', { follow: mainTwitterId });
 
-  Twitter.on('connection success', (uri) => {
+  stream.on('connection success', (uri) => {
     console.log('Twitter Stream - Connected', uri);
   });
 
-  Twitter.on('connection aborted', () => {
+  stream.on('connection aborted', () => {
     console.log('Twitter Stream - Connection Aborted');
     handleSetupError(new Error('Connection Aborted'));
   });
 
-  Twitter.on('data', (obj) => {
-    // Typecast and parse accordingly as JSON
-    const tweet = JSON.parse(obj.toString('utf8'));
-    console.log(tweet.text);
-    console.log('----');
-    updateCachedTweetsByAthleteId(1, tweet);
+  stream.on('data', (tweet) => {
+    // Filter out tweets to only those coming from Spencer himself
+    if (tweet.user.id_str === mainTwitterId) {
+      console.log(tweet.text);
+      console.log('----');
+      updateCachedTweetsByAthleteId(1, tweet);
+    }
   });
 
   // Sort of like "PING" from Twitter to indicate that the connection is still alive
-  Twitter.on('data keep-alive', () => {
+  stream.on('data keep-alive', () => {
     console.log('Twitter Stream - Keep-alive');
   });
 
-  Twitter.on('data error', (error) => {
+  stream.on('data error', (error) => {
     console.log('Twitter Stream - Error', error);
     handleSetupError(error);
   });
 
-  Twitter.on('connection rate limit', (httpStatusCode) => {
+  stream.on('connection rate limit', (httpStatusCode) => {
     console.log('Twitter Stream - Rate Limit Error', httpStatusCode);
     handleSetupError(new Error(`Connection Rate Limited: ${httpStatusCode}`));
   });
 
-  Twitter.on('connection error network', (error) => {
+  stream.on('connection error network', (error) => {
     console.log('Twitter Stream - Network Error', error);
     handleSetupError(error);
   });
 
-  Twitter.on('connection error http', (httpStatusCode) => {
+  stream.on('connection error http', (httpStatusCode) => {
     console.log('Twitter Stream - HTTP Error', httpStatusCode);
     handleSetupError(`HTTP Error: ${httpStatusCode}`);
   });
 
-  Twitter.on('connection error stall', () => {
+  stream.on('connection error stall', () => {
     console.log('Twitter Stream - Connection Stalled');
     handleSetupError(new Error('Connection Stalled'));
   });
 
-  Twitter.on('connection error unknown', (error) => {
+  stream.on('connection error unknown', (error) => {
     console.log('Twitter Stream - Unknown Error', error);
     handleSetupError(error);
   });
