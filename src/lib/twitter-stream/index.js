dd4b26865f9de1aedceac344ce3b327d5e4e3ada
Eko Mirhard <eko.mirhard@gmail.com>
2019-09-27 00:28:34 +0700

    Switch to pull mechanism

--- src/lib/twitter-stream/index.js
+++ src/lib/twitter-stream/index.js
@@ -1,5 +1,8 @@
 const Twitter = require('twitter');
-const { updateCachedTweetsByAthleteId } = require('../redis/twitter');
+const {
+  updateCachedTweetsByAthleteId,
+  getLastTweetByAthleteId,
+} = require('../redis/twitter');
 const throwError = require('../../util/throw-error');
 
 const {
@@ -16,73 +19,32 @@ const credentials = {
   access_token_secret: TWITTER_ACCESS_TOKEN_SECRET,
 };
 
-const handleSetupError = (err) => {
-  throwError(err, {
-    fn: 'setupTwitterStream',
-    source: 'src/lib/twitter-stream/index.js',
-  }, {}, false);
-};
-
 // Initiate the tweet streaming from Twitter API
 const setupTwitterStream = () => {
   const client = new Twitter(credentials);
   const mainTwitterId = '368148234';
+  const fetchInterval = 10 * 1000; // set to 10 seconds
 
-  // Setup the streamer and follow Spencer's account
-  const stream = client.stream('statuses/filter', { follow: mainTwitterId });
-
-  stream.on('connection success', (uri) => {
-    console.log('Twitter Stream - Connected', uri);
-  });
-
-  stream.on('connection aborted', () => {
-    console.log('Twitter Stream - Connection Aborted');
-    handleSetupError(new Error('Connection Aborted'));
-  });
-
-  stream.on('data', (tweet) => {
-    // Filter out tweets to only those coming from Spencer himself
-    if (tweet.user.id_str === mainTwitterId) {
-      console.log(tweet.text);
-      console.log('----');
-      updateCachedTweetsByAthleteId(1, tweet);
+  // Periodically fetch and sync Spencer's latest tweets to our local storage
+  setInterval(async () => {
+    try {
+      // Only fetch the latest 100 tweets after the last one already saved locally
+      const lastTweet = await getLastTweetByAthleteId(1);
+      const params = { user_id: mainTwitterId, count: 100, since_id: lastTweet.id };
+      const tweets = await client.get('statuses/user_timeline', params);
+      console.log('New Tweets: ', tweets.length);
+      tweets.forEach((tweet) => {
+        console.log('--------');
+        console.log(tweet.text);
+        updateCachedTweetsByAthleteId(1, tweet);
+      });
+    } catch (err) {
+      throwError(err, {
+        fn: 'setupTwitterStream',
+        source: 'src/lib/twitter-stream/index.js',
+      }, {}, false);
     }
-  });
-
-  // Sort of like "PING" from Twitter to indicate that the connection is still alive
-  stream.on('data keep-alive', () => {
-    console.log('Twitter Stream - Keep-alive');
-  });
-
-  stream.on('data error', (error) => {
-    console.log('Twitter Stream - Error', error);
-    handleSetupError(error);
-  });
-
-  stream.on('connection rate limit', (httpStatusCode) => {
-    console.log('Twitter Stream - Rate Limit Error', httpStatusCode);
-    handleSetupError(new Error(`Connection Rate Limited: ${httpStatusCode}`));
-  });
-
-  stream.on('connection error network', (error) => {
-    console.log('Twitter Stream - Network Error', error);
-    handleSetupError(error);
-  });
-
-  stream.on('connection error http', (httpStatusCode) => {
-    console.log('Twitter Stream - HTTP Error', httpStatusCode);
-    handleSetupError(`HTTP Error: ${httpStatusCode}`);
-  });
-
-  stream.on('connection error stall', () => {
-    console.log('Twitter Stream - Connection Stalled');
-    handleSetupError(new Error('Connection Stalled'));
-  });
-
-  stream.on('connection error unknown', (error) => {
-    console.log('Twitter Stream - Unknown Error', error);
-    handleSetupError(error);
-  });
+  }, fetchInterval);
 };
 
 module.exports = {
